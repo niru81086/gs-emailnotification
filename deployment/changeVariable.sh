9fb895a46cf97b538686a4514512f887637ddaa8
naresh <niru81086@gmail.com>
2021-09-14 17:12:49 +0530

    adding QA code

--- deployment/changeVariable.sh
+++ deployment/changeVariable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed  "s/imagename/$1\/$2/g" ../deployment/email-notification.yaml > ../deployment/$2.yaml
+sed  "s/imagename/$1\/$2/g" deployment/email-notification.yaml > deployment/$2.yaml
