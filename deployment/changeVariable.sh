67950d5eea87ebed5c30410bb7836e425ac07be8
niru81086 <niru81086@gmail.com>
2021-09-17 15:19:30 +0530

    modify jenkinsfile as per branch statergy

--- deployment/changeVariable.sh
+++ deployment/changeVariable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed  "s/imagename/$1\/$2/g" deployment/email-notification.yaml > deployment/hhhh.yaml
+sed  "s/imagename/$1\/$2/g" deployment/email-notification.yaml > deployment/$2:$3.yaml
 sed  "s/nodeport/$4/g" deployment/rabbitmq-deploy.yaml > deployment/$5-rabbitmq-deploy.yaml
 
 echo $3
